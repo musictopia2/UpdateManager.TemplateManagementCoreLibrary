073676cf3fd200e18598ac1e5a480de17019512f
Andy Walter <musictopia2@hotmail.com>
2025-10-27 18:19:26 -0700

    Renamed and deleted service registration.

--- ServiceExtensions.cs
+++ ServiceExtensions.cs
@@ -2,18 +2,13 @@
 public static class ServiceExtensions
 {
     //this is not going to be a post build.   this time, has to run a separate app for this.
-    public static IServiceCollection RegisterUpdateTemplateServices(this IServiceCollection services)
+    public static IServiceCollection RegisterDiscoverUpdateTemplateServices(this IServiceCollection services)
     {
         services.AddTransient<ITemplatesContext, FileTemplatesContext>()
             .AddSingleton<INugetTemplatePacker, NugetPacker>()
             .AddSingleton<PrivateTemplateDeploymentProcessor>()
-            ; //for now, just one.
-        return services;
-    }
-    public static IServiceCollection RegisterTemplateDiscoveryServices(this IServiceCollection services)
-    {
-        services.AddTransient<ITemplatesContext, FileTemplatesContext>()
             .AddTransient<TemplateDiscoveryService>();
+        ; //for now, just one.
         return services;
     }
     public static IServiceCollection RegisterPublicTemplateUploadServices(this IServiceCollection services)
